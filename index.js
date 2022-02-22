e712bd982780b97d4baca883a62ea6bf6a1fef39
Nafis-22 <98020183+Nafis-22@users.noreply.github.com>
2022-02-22 13:27:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 function increment()
 {
     let demo = document.getElementById('demo');
-    let count = parseInt(votes.innerHTML); 
+    let count = parseInt(demo.innerHTML); 
     count++; 
-    votes.innerHTML = count;   
+    demo.innerHTML = count;   
 }
 
 function decrement()
 {
     let demo = document.getElementById('demo');
-    let count = parseInt(votes.innerHTML); 
+    let count = parseInt(demo.innerHTML); 
     count--; 
-    votes.innerHTML = count;   
+    demo.innerHTML = count;   
 }
